ba7317d8c54ba1a1754233fda5593a2fd66e51f3
nathaliasafada667-prog <nathaliasafada667@gmail.com>
2026-01-24 10:30:46 -0300

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,5 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@^2.39.0';
+
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xrkgbsdwfmhvokigkonx.supabase.co';
 const supabaseAnonKey = 'sb_publishable_9_1gh6U73z6zXnMIyx1MTg_J-m7jKzN';
