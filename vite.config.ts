83e3489b0416872562da04070282abe1bfae171b
nathaliasafada667-prog <nathaliasafada667@gmail.com>
2026-01-24 20:33:12 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   define: {
-    // Garante que a API_KEY da Vercel esteja disponível no navegador
+    // Garante que a API_KEY da Vercel esteja disponível no código cliente
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
   build: {
