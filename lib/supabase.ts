d9b00ec8f462b4f6e31d9fec406db6269ea77f78
nathaliasafada667-prog <nathaliasafada667@gmail.com>
2026-01-25 20:51:06 -0300

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,4 +4,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://dmieubfmyiznrctzqkry.supabase.co';
 const supabaseAnonKey = 'sb_publishable_UCDlXOjJJBDDv_MQuVTAdw_4Hwk9omK';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true, // Garante que a sessão seja salva no LocalStorage
+    autoRefreshToken: true, // Renova o token automaticamente
+    detectSessionInUrl: true // Útil para links de confirmação de e-mail
+  }
+});
