01466a6d0dd9b60c82ee8131f555b52e755b070a
nathaliasafada667-prog <nathaliasafada667@gmail.com>
2026-01-27 19:32:41 -0300

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.esmael.store',
+  appName: 'Esmael Store',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
